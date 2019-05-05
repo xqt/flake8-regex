9fef66718ce8449b95b599ec51b2dc78f4dab046
xqt <info@gno.de>
2019-05-05 14:38:19 +0200

    [IMPR] Some improvements
    setup.cfg
    - set max-lin-length to 80
    
    setup.py
    - Add some header informations
    - retrieve __version__ directly
    - update classifiers
    - update setup parameters
    
    flake8_regex.py
    - Add some header informations
    - remove pep8 dependeny
    - rewrite str.format()
    - use single quotes

--- flake8_regex.py
+++ flake8_regex.py
@@ -1,12 +1,19 @@
-__version__ = '0.4'
+# -*- coding: utf-8 -*-
+"""Define a flake8 plugin to check for any given regex."""
+#
+# Distributed under the terms of the MIT license.
+#
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+
+__version__ = '1.0'
 
 import importlib
 import os
 import sys
-import pep8
 
 sys.path.append(os.getcwd())
-CONFIG_MODULE = os.environ.get("FLAKE8_REGEX_CONFIG_MODULE")
+CONFIG_MODULE = os.environ.get('FLAKE8_REGEX_CONFIG_MODULE')
 
 PATTERNS_CONFIG = (getattr(importlib.import_module(CONFIG_MODULE), 'rules')
                    if CONFIG_MODULE else None)
@@ -14,14 +21,12 @@ PATTERNS_CONFIG = (getattr(importlib.import_module(CONFIG_MODULE), 'rules')
 
 def check_regex_patterns(physical_line):
     if PATTERNS_CONFIG:
-        if pep8.noqa(physical_line):
-            return
-
         for pattern_cfg in PATTERNS_CONFIG:
             match = pattern_cfg['regex'].search(physical_line)
             if match:
                 return (match.start(),
-                        str.format('R{} {}', pattern_cfg['code'], pattern_cfg['message']))
+                        'R{} {}'.format(pattern_cfg['code'],
+                                        pattern_cfg['message']))
 
 
 check_regex_patterns.name = name = 'flake8-regex'
--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
-# coding: utf-8
-
-from __future__ import with_statement
+# -*- coding: utf-8 -*-
+"""Arbitrary regex checker, extension for flake8."""
+#
+# Distributed under the terms of the MIT license.
+#
+from __future__ import absolute_import, print_function, unicode_literals
 from setuptools import setup
-
-
-def get_version(fname='flake8_regex.py'):
-    with open(fname) as f:
-        for line in f:
-            if line.startswith('__version__'):
-                return eval(line.split('=')[-1])
-
+from flake8_regex import __version__ as version
 
 def get_long_description():
     descr = []
@@ -18,22 +14,22 @@ def get_long_description():
             descr.append(f.read())
     return '\n\n'.join(descr)
 
-
 setup(
     name='flake8-regex',
-    version=get_version(),
-    description="Arbitrary regex checker, extension for flake8",  # noqa
+    license='MIT License',
+    version=version,
+    description='Arbitrary regex checker, extension for flake8',
     long_description=get_long_description(),
-    keywords='flake8 regex',
+    keywords=('flake8', 'regex'),
     author='Aristide Niyungeko',
     author_email='aristide.niyungeko@gmail.com',
     url='https://github.com/aristiden7o/flake8-regex',
-    license='GNU General Public License v2 (GPLv2)',
+    python_requires='>=2.7.4, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     py_modules=['flake8_regex'],
-    zip_safe=False,
+    install_requires=['flake8 >= 3.6.0'],
     entry_points={
         'flake8.extension': [
-            'flake8_regex = flake8_regex:check_regex_patterns',
+            'R = flake8_regex:check_regex_patterns',
         ],
     },
     classifiers=[
@@ -44,8 +40,15 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Utilities',
     ],
 )
